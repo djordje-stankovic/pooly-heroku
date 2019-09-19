cdc63eb46a7cb70820dba0f12741a6970c17ef76
djordje-stankovic <djordje.stankovic93@gmail.com>
2019-09-19 23:19:40 +0200

    Fix

--- server_express/app.js
+++ server_express/app.js
@@ -40,7 +40,7 @@ app.post("/signin", userControler.login);
 app.post("/markVoteOnPool", userControler.markVoteOnPool);
 
 //TODO: PRoveriti da li ovde treba /
-app.get('*', (req,res) =>{
+app.get('/', (req,res) =>{
   res.sendFile(path.join(__dirname, '../pooly/build/index.html'));
 });
 
